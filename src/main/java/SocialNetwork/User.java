0eb22f3cd7ca8fa598e9a1e9d32b88c8c2b91352
Shagil <shagilsid@gmail.com>
2018-12-28 09:34:56 +0530

    extra commented lines deleted

--- src/main/java/SocialNetwork/User.java
+++ src/main/java/SocialNetwork/User.java
@@ -35,16 +35,6 @@ public class User extends  GuestUser{
     }
 
     public void followRequest(User otherUser) {
-//        FollowRequest sentrequest=new FollowRequest();
-//        sentrequest.start=1;
-//        sentrequest.startingUser=this;
-//        sentrequest.endingUser=otherUser;
-//        this.followRequestsList.add(sentrequest);
-//        FollowRequest receivedRequest=new FollowRequest();
-//        receivedRequest.end=1;
-//        receivedRequest.endingUser=this;
-//        receivedRequest.startingUser=otherUser;
-//        otherUser.followRequestsList.add(receivedRequest);
         FollowRequest request=new FollowRequest();
         request.end=1;
         request.startingUser=this;
@@ -58,18 +48,11 @@ public class User extends  GuestUser{
 
         for (FollowRequest request :
                 followRequestsList) {
-//            System.out.printf("%d. %s: %s \n",++i,(request.end==1)?"From":"To",request.endingUser.userName);
             boolean isTo=(request.startingUser.equals(this)&&request.end==1);
-//            System.out.printf("%d. %s: %s\n",++i,(isTo)?"To":"From",(isTo)?request.endingUser.userName:request.startingUser.userName);
             if (isTo){
                 System.out.printf("%d. To: %s\n",followRequestsList.indexOf(request),request.endingUser.userName);
             }
         }
-//        System.out.println("Do you want to delete any sent request? Then enter the desired index(num/n)");
-//        Scanner input=new Scanner(System.in);
-//        int index=input.nextInt();
-//        System.out.println(followRequestsList.remove(index));
-//
     }
 
     public void viewRecievedRequests() {
@@ -77,9 +60,7 @@ public class User extends  GuestUser{
 
         for (FollowRequest request :
                 followRequestsList) {
-//            System.out.printf("%d. %s: %s \n",++i,(request.end==1)?"From":"To",request.endingUser.userName);
             boolean isFrom=(request.endingUser.equals(this)&&request.end==1);
-//            System.out.printf("%d. %s: %s\n",++i,(isTo)?"To":"From",(isTo)?request.endingUser.userName:request.startingUser.userName);
             if (isFrom){
                 System.out.printf("%d. From: %s\n",followRequestsList.indexOf(request),request.startingUser.userName);
             }
